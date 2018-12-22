e14f680f19d9319b60c21d8de9d0452b692d7167
qixin <qixin@haoqix.com>
2018-12-22 21:14:10 +0800

    wkwebview demo update

--- iOSDemo/containers/home/WkWebDemoViewController.h
+++ iOSDemo/containers/home/WkWebDemoViewController.h
@@ -4,6 +4,8 @@
 //
 //  Created by qixin on 2018/12/22.
 //  Copyright © 2018年 qixin. All rights reserved.
+//
+// 始末 为了解决iOS wkwebView 出现白屏问题  iOS12 https://www.cnblogs.com/soulDn/p/9700254.html
 
 // 参考  https://www.jianshu.com/p/e5af3a2127ce 入门，只做了wkwebview初始化
 //
